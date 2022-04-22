be6dd95634fffda1e76bf203d3b3d40426d57f96
IT20131456 <it20131456@my.sliit.lk>
2022-04-23 03:17:23 +0530

    Second Commit

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
+//import controllers
+const controller = require("./controllers/controllers");
+const router = require("./controllers/controllers");
+
+//app middleware
+
+app.use(bodyparser.json());
+app.use(cors());
+
+app.use(router);
+
 const PORT = 8000;
-const DB_URI = 'mongodb+srv://chamaka:mern123@cluster0.f7pyo.mongodb.net/merndb?retryWrites=true&w=majority';
+const DB_URI =
+  "mongodb+srv://Hasinda:p63gMesJIsbnbtTS@rpmt.txmdj.mongodb.net/rpmtdb?retryWrites=true&w=majority";
 
-mongoose.connect(DB_URI,{
+mongoose
+  .connect(DB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(()=>{
-    console.log('DB Connected');
-})
-.catch((error) => console.log('DB Connection Error',error));
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((error) => console.log("DB Connection Error", error));
 
-
-
-app.listen(PORT,()=>{
-    console.log(`App is running on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
 });
