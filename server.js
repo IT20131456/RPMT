53a65dfd8c44a40b93ce0ba5af8b6aade3285f81
IT20131456 <it20131456@my.sliit.lk>
2022-04-23 12:17:02 +0530

    Fourth Commit

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
-const express = require("express");
+const express = require('express');
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 const cors = require("cors");
 
+require("dotenv").config();
+
 const app = express();
 
 //import controllers
-const controller = require("./controllers/controllers");
-const router = require("./controllers/controllers");
+//const controller = require("./controllers/controllers");
+//const router = require("./controllers/controllers");
 
 //app middleware
 
 app.use(bodyparser.json());
 app.use(cors());
 
-app.use(router);
 
-const PORT = 8000;
-const DB_URI =
-  "mongodb+srv://Hasinda:p63gMesJIsbnbtTS@rpmt.txmdj.mongodb.net/rpmtdb?retryWrites=true&w=majority";
+//app.use(router);
 
-mongoose
-  .connect(DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("DB Connected");
-  })
-  .catch((error) => console.log("DB Connection Error", error));
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
 
-app.listen(PORT, () => {
-  console.log(`App is running on ${PORT}`);
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connected");
+});
+
+app.listen(port, () => {
+  console.log(`serever is started in port ${port}`);
 });
