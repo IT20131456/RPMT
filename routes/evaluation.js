8782d8d2dd93f49c99e4b2dfefd2ef7985ef2c17
IT20128036 <it20128036@my.sliit.lk>
2022-05-01 14:15:39 +0530

    new

--- routes/evaluation.js
+++ routes/evaluation.js
@@ -57,7 +57,7 @@ router.get("/evaluation/:id",(req,res) =>{
 });
 
 
-//update  evaluation
+//update evaluation
 router.put('/evaluation/update/:id',(req,res)=>{
     Evaluations.findByIdAndUpdate(
         req.params.id,
