0184fd680b8e74b58b0c159249ecc19e06e68230
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-05-26 13:04:53 +0530

    Update create marking page

--- frontend/src/components/IT20127046/CreateMarkingSchem.js
+++ frontend/src/components/IT20127046/CreateMarkingSchem.js
@@ -12,6 +12,7 @@ export default class CreateMarkingSchem extends Component {
       allocateMark: "",
       markingSchemTitleID: "",
       markingCriteria: [],
+      addCriteriaBtn: true
     };
   }
 
@@ -49,6 +50,7 @@ export default class CreateMarkingSchem extends Component {
 
         this.setState({
           markingSchemTitleID: res.data.saveTitle,
+          addCriteriaBtn: false
         });
       }
     });
@@ -117,95 +119,104 @@ export default class CreateMarkingSchem extends Component {
   render() {
     return (
       <div className="container">
-          <br/>
-          <AdminNavBar />
-          <br/>
+        <br />
+        <AdminNavBar />
+        <br />
         <h4>Create Marking Schem</h4>
         <hr />
 
-        <form onSubmit={this.onSubmitTitle}>
-          <h4>Set Marking Schem Title</h4>
+        <div className="container p-3 mb-2 bg-light text-dark">
           <div className="row">
-            <div className="form-group col">
-              <label htmlFor="moduleName">Module</label>
-              <input
-                style={{ width: "400px" }}
-                type="text"
-                className="form-control"
-                id="moduleName"
-                name="moduleName"
-                value={this.state.moduleName}
-                onChange={this.handleInputChange}
-              />
+            <div className="col">
+              <div className="border border-secondary m-2 p-4 rounded">
+                <form onSubmit={this.onSubmitTitle}>
+                  <h4>Set Marking Schem Title</h4>
+
+                  <label htmlFor="moduleName">Module</label>
+                  <input
+                    style={{ width: "400px" }}
+                    type="text"
+                    className="form-control"
+                    id="moduleName"
+                    name="moduleName"
+                    value={this.state.moduleName}
+                    onChange={this.handleInputChange}
+                  />
+
+                  <label htmlFor="assignment">Assignment</label>
+                  <input
+                    style={{ width: "400px" }}
+                    type="text"
+                    className="form-control"
+                    id="assignment"
+                    name="assignment"
+                    value={this.state.assignment}
+                    onChange={this.handleInputChange}
+                  />
+
+                  <button type="submit" className="btn btn-outline-primary mt-2 mb-2">
+                    Set Title&nbsp; <i class="fa fa-plus" aria-hidden="true"></i>
+                  </button>
+                </form>
+              </div>
             </div>
-            <div className="form-group col">
-              <label htmlFor="assignment">Assignment</label>
-              <input
-                style={{ width: "400px" }}
-                type="text"
-                className="form-control"
-                id="assignment"
-                name="assignment"
-                value={this.state.assignment}
-                onChange={this.handleInputChange}
-              />
+            <div className="col">
+              <div className="border border-secondary m-2 p-4 rounded">
+                <form onSubmit={this.onSubmit}>
+                  <h4>Set Marking Schem Criteria</h4>
+
+                  <label htmlFor="criteria">Criteria</label>
+                  <input
+                    style={{ width: "400px" }}
+                    type="text"
+                    className="form-control"
+                    id="criteria"
+                    name="criteria"
+                    value={this.state.criteria}
+                    onChange={this.handleInputChange}
+                  />
+
+                  <label htmlFor="allocateMark">Allocate Mark</label>
+                  <input
+                    style={{ width: "400px" }}
+                    type="text"
+                    className="form-control"
+                    id="allocateMark"
+                    name="allocateMark"
+                    value={this.state.allocateMark}
+                    onChange={this.handleInputChange}
+                  />
+
+                  <button type="submit" className="btn btn-outline-primary mt-2 mb-2" disabled={this.state.addCriteriaBtn}>
+                    Add Criteria&nbsp; <i class="fa fa-plus" aria-hidden="true"></i>
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
-          <button type="submit" className="btn btn-secondary mt-2 mb-2">
-            Set Title
-          </button>
-          <br />
-          <br />
-        </form>
-
-        <form onSubmit={this.onSubmit}>
-          <h4>Set Marking Schem Criteria</h4>
-          <div className="row">
-            <div className="form-group col">
-              <label htmlFor="criteria">Module</label>
-              <input
-                style={{ width: "400px" }}
-                type="text"
-                className="form-control"
-                id="criteria"
-                name="criteria"
-                value={this.state.criteria}
-                onChange={this.handleInputChange}
-              />
-            </div>
-            <div className="form-group col">
-              <label htmlFor="allocateMark">Assignment</label>
-              <input
-                style={{ width: "400px" }}
-                type="text"
-                className="form-control"
-                id="allocateMark"
-                name="allocateMark"
-                value={this.state.allocateMark}
-                onChange={this.handleInputChange}
-              />
-            </div>
-          </div>
-
-          <button type="submit" className="btn btn-primary mt-2 mb-2">
-            Add Criteria <i className="fa-thin fa-plus"></i>
-          </button>
-        </form>
+        </div>
 
         <br />
         <div className="container border border-secondary">
-            <center>
-                <h5>Marking Schem</h5>
-                <h5>{this.state.moduleName}</h5>
-                <h6>{this.state.assignment}</h6>
-            </center>
+          <div className="container p-4">
+          <center>
+            <h5>Marking Schem</h5>
+            <h5>{this.state.moduleName}</h5>
+            <h6>{this.state.assignment}</h6>
+          </center>
 
-          <table className="table ">
+          <table className="table">
             <thead>
               <tr className="b ">
-                <th scope="col"><center>No</center></th>
-                <th scope="col"><center>Criteris</center></th>
-                <th scope="col"><center>Allocate Mark</center></th>
+                <th scope="col">
+                  <center>No</center>
+                </th>
+                <th scope="col">
+                  <center>Criteris</center>
+                </th>
+                <th scope="col">
+                  <center>Allocate Mark</center>
+                </th>
               </tr>
             </thead>
 
@@ -233,6 +244,9 @@ export default class CreateMarkingSchem extends Component {
               ))}
             </tbody>
           </table>
+          <br/>
+          <br/>
+          </div>
         </div>
       </div>
     );
